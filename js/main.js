78c331e95cde2b6695dba48ba1d6f957d0662d49
westone1 <diegoberde18@gmail.com>
2025-04-02 12:56:25 -0500

    CAMBIO DE TEMA
    Se agrego la primera etapa del cambio de tema(Tema claro / tema oscuro)

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const cinta = document.querySelector(".brands__mainContainer__slider__cinta")
+
+/*CARRUSEL DE MARCAS*/
+const cinta = document.querySelector(".brands__mainContainer__slider__cinta") 
 let posicion = 0;
 
 
@@ -12,3 +14,35 @@ function carrusel(){
     }
 }
 setInterval(carrusel,500)
+
+/*CAMBIO DE TEMA(CLARO / OSCURO)*/ 
+const themeButton = document.getElementById("themeButton")
+let flagTheme = 0
+function switchTheme(){
+    if(flagTheme == 0){
+        temaOscuro()
+        flagTheme = 1;
+        return flagTheme
+    }
+    if(flagTheme == 1){
+        temaClaro()
+        flagTheme = 0;
+        return flagTheme
+    }
+}
+function temaOscuro(){
+    themeButton.classList.remove(`fa-moon`)
+    themeButton.classList.add(`fa-sun`)
+
+    const header = document.getElementById("header")
+    header.style.backgroundColor="black"
+
+    const linksMenuBar = document.querySelectorAll("#link_menuBar")
+   for(let i = 0; i<linksMenuBar.length;i++){
+        linksMenuBar[i].style.color="white"
+   }
+}
+function temaClaro(){
+    themeButton.classList.remove(`fa-sun`)
+    themeButton.classList.add(`fa-moon`)  
+}
