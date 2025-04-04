ea16bce5b88bb0d8dd6237e091d110cc44486008
westone1 <diegoberde18@gmail.com>
2025-04-03 21:17:36 -0500

    CAMBIO DE TEMA
    Se completo el cambio de tema(tema claro/tema oscuro) en todo lo que va del proyecto

--- js/main.js
+++ js/main.js
@@ -30,19 +30,55 @@ function switchTheme(){
         return flagTheme
     }
 }
+
+const header = document.getElementById("header")
+const linksMenuBar = document.querySelectorAll("#link_menuBar")
+const articles = document.querySelectorAll("#whiteArticle")
+const whiteTexts = document.querySelectorAll("#whiteText")
+const whiteShape = document.querySelectorAll("#whiteShape")
+
 function temaOscuro(){
     themeButton.classList.remove(`fa-moon`)
     themeButton.classList.add(`fa-sun`)
 
-    const header = document.getElementById("header")
-    header.style.backgroundColor="black"
-
-    const linksMenuBar = document.querySelectorAll("#link_menuBar")
-   for(let i = 0; i<linksMenuBar.length;i++){
-        linksMenuBar[i].style.color="white"
+    
+    header.style.backgroundColor="var(--color-negro)"
+    for(let i = 0; i<linksMenuBar.length;i++){
+        linksMenuBar[i].style.color="var(--color-blanco)"
+    }
+    for(let i = 0; i<articles.length; i++){
+        articles[i].style.backgroundColor="var(--color-negro)"
+    }
+    for(let i = 0; i<whiteTexts.length; i++){
+        whiteTexts[i].style.color="var(--color-blanco)"
+    }
+    for(let i = 0; i<whiteShape.length;i++){
+        whiteShape[i].style.backgroundColor="var(--color-negro)"
    }
+    
+   
 }
 function temaClaro(){
     themeButton.classList.remove(`fa-sun`)
-    themeButton.classList.add(`fa-moon`)  
+    themeButton.classList.add(`fa-moon`)
+
+    header.style.backgroundColor="var(--color-blanco)"
+    for(let i = 0; i<linksMenuBar.length;i++){
+        linksMenuBar[i].style.color="var(--color-plomo-letras)"
+    }
+    for(let i = 0; i<articles.length; i++){
+        articles[i].style.backgroundColor="var(--color-plomo-fondo)"
+    }
+    for(let i = 0; i<whiteTexts.length; i++){
+        whiteTexts[i].style.color="var(--color-plomo-letras)"
+    }
+    for(let i = 0; i<whiteShape.length;i++){
+        whiteShape[i].style.backgroundColor="var(--color-plomo-fondo)"
+   }
+    
+
 }
+/*MODAL*/
+function setModal(){
+    
+} 
